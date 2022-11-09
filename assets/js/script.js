e2a76656fe54b81a0771fd6b8125aebe0560a21a
bruno <bpires.siqueira@gmail.com>
2022-11-09 06:35:07 -0300

    update branch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,23 @@
-const limparFormulario = (endereco) => {
+const preencherFormulario = (endereco) => {
     document.getElementById('logradouro').value = endereco.logradouro;
     document.getElementById('bairro').value = endereco.bairro;
     document.getElementById('cidade').value = endereco.localidade;
     document.getElementById('estado').value = endereco.uf;
 };
 
-const preencherFormulario = (endereco) => {
+const limparFormulario = () => {
     document.getElementById('logradouro').value = '';
     document.getElementById('bairro').value = '';
     document.getElementById('cidade').value = '';
     document.getElementById('estado').value = '';
 };
 //validação do campo do cep
+//validação do campo do cep ---- esta parecendo o input tenta alterar para o span, que fica melhor,(se de tempo)
 const cepValido = (cep) => cep.length === 8 && /^[0-9]+$/.test(cep);
 const pesquisarCep = async () => {
     limparFormulario();
-    const cep = document.getElementById('cep').value;
-    const url = `http://viacep.com.br/ws/${cep}/json/`;
-    if (cepValido(cep)) {
-        const dados = await fetch(url); //recebendo o valor do fetch
-        const endereco = await dados.json(); //pegando os dados e atribuindo a função json
-        //tratando erro,caso for apresentado um cep que nao tenha, vai ser avisado ao usuario
-        if (endereco.hasOwnProperty('erro')) {
-            document.getElementById('logradouro').value = 'Cep não encontrado!';
-        } else {
-            preencherFormulario(endereco);
-        }
-    } else {
-        document.getElementById('cep').value = 'Cep incorreto!';
-    }
+    const pesquisarCep = async () => {};
+    pesquisarCep();
 };
 
-document.getElementById('cep').addEventListener('focusout', pesquisarCep); //quando sai do foco do input ele automaticamente inicia.
+document.getElementById('cep').addEventListener('focusout', pesquisarCep);
