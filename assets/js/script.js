efe24e22849019a115f1d7037991c66c4c576105
Jéssica Ramos <111541621+jessicarsmos@users.noreply.github.com>
2022-11-09 00:58:32 -0300

    add modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,17 @@
-const limparFormulario = (endereco) => {
+const preencherFormulario  = (endereco) => {
     document.getElementById("logradouro").value = endereco.logradouro;
     document.getElementById("bairro").value = endereco.bairro;
     document.getElementById("cidade").value = endereco.localidade;
     document.getElementById("estado").value = endereco.uf;
 }
 
-const preencherFormulario = (endereco) => {
+const limparFormulario = (endereco) => {
     document.getElementById("logradouro").value = "";
     document.getElementById("bairro").value = "";
     document.getElementById("cidade").value = "";
     document.getElementById("estado").value = "";
 }
-//validação do campo do cep 
+//validação do campo do cep ---- esta parecendo o input tenta alterar para o span, que fica melhor,(se de tempo)
 const cepValido = (cep) => cep.length === 8 && /^[0-9]+$/.test(cep);
 const pesquisarCep = async() => {
     limparFormulario();
@@ -32,3 +32,22 @@ const pesquisarCep = async() => {
 }
 
 document.getElementById('cep').addEventListener("focusout", pesquisarCep);//quando sai do foco do input ele automaticamente inicia.
+
+//modal----- o modal quando chega a 768 da uma quebrada/ quando aperta no botão vai para uma pg de pastas/botei para aparecer o modal toda vez que apertar o botão
+function iniciaModal(modalID){
+    const modal = document.getElementById(modalID);
+    if(modal){
+        modal.classList.add('mostrar');
+        modal.addEventListener('click', (Event) =>{
+            if(e.target.id == modalID){
+                modal.classList.remove('mostrar');
+            }
+        })
+    }
+}
+
+const cadastrar = document.querySelector(".button");
+cadastrar.addEventListener("click", function(){
+    iniciaModal("modal-container");
+});
+
